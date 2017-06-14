8610a9735be964161dc3053d7a46dbe702696df1
txd825 <txd825@gmail.com>
2017-06-14 20:12:59 +0800

    实现触摸

---
+++ New Unity Project/Assets/Scripts/UI_Main_ShowDebug.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class UI_Main_ShowDebug : MonoBehaviour {
+
+
+
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+	}
+
+
+    //void OnGUI()
+    //{
+
+    //    GUI.Box(new Rect(400, 5, 200, 50), "show me");
+    //}
+}
